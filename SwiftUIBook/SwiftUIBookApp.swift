21dce6e1fc31c1a4f41828f5cfc65cb8db3c24b6
SHUBHAM BISWAS <suvm.dev@gmail.com>
2024-01-18 20:45:08 +0530

    Initial Commit

---
+++ SwiftUIBook/SwiftUIBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBookApp.swift
+//  SwiftUIBook
+//
+//  Created by Shubham Biswas on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
